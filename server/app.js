bc5fed0aa98e4575f3127caef5f4a1e3e0452155
essemd <sandimilicevic1991@gmail.com>
2022-03-13 14:07:57 -0400

    separate config from rtmp server code

--- server/app.js
+++ server/app.js
@@ -1,19 +1,5 @@
 const NodeMediaServer = require('node-media-server');
-//const config = require('./config/default').rtmp_server;
-
-const config = {
-    rtmp: {
-        port: 1935,
-        chunk_size: 60000,
-        gop_cache: true,
-        ping: 30,
-        ping_timeout: 60
-    },
-    http: {
-        port: 8000,
-        allow_origin: '*'
-    }
-};
+const config = require('./config');
 
 let nms = new NodeMediaServer(config);
 nms.run();
