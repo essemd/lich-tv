06f4fa3f93fe99e2f00372f54eb20fe594121e5c
essemd <sandimilicevic1991@gmail.com>
2022-03-13 13:49:35 -0400

    implement bare bones rtmp server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 60000,
+        gop_cache: true,
+        ping: 30,
+        ping_timeout: 60
+    }
+};
+
+let nms = new NodeMediaServer(config);
+nms.run();
