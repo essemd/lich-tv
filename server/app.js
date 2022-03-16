68d804605f5f409a53256e08b3b36c5d3bc94281
essemd <sandimilicevic1991@gmail.com>
2022-03-15 20:23:53 -0400

    hook up passport.authenticate middleware (the authentication currently does nothing)

--- server/app.js
+++ server/app.js
@@ -1,11 +1,29 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser'); // need this if we're going to be POSTing
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
 const port = 5000;
 
-app.get('/', function(req, res) {
-    res.send("Hello world!");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        console.log("We do nothing as a strategy!");
+    }));
+
+app.get('/login',
+  function(req, res) {
+    res.sendFile('views/loginform.html', {root: __dirname});
+  });
+
+app.post('/login/password', passport.authenticate('local'),
+    function(req, res) {
+        res.send('Hello world!');
 });
 
 app.listen(port, () => {
-    console.log("Server up and running...");
+    console.log('Server up and running...');
 });
