cc5f872936a7165d55a400a48cfabcdbc6df5767
essemd <sandimilicevic1991@gmail.com>
2022-04-06 15:46:25 -0400

    implement thumbnail refreshing

--- client/src/components/streamLink.js
+++ client/src/components/streamLink.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
 export default function StreamLink(props) {
-    const imgSrc = 'http://localhost:5000/thumbnails/' + props.streamId + '.png';
+    const [imgSrc, setImg] = useState();
+
+    const imgUrl = 'http://localhost:5000/thumbnails/' + props.streamId + '.png';
     const pathname = 'stream/' + props.streamId;
+
+    const fetchImage = async () => {
+        const res = await fetch(imgUrl);
+        const imageBlob = await res.blob();
+        const imageObjectURL = URL.createObjectURL(imageBlob); // object URL should be freed explicitly
+
+        setImg(imageObjectURL);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(fetchImage, 5001);
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="d-flex justify-content-center">
