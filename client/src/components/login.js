8526ff5ec7caf1604629771d52e42098d5325b0c
essemd <sandimilicevic1991@gmail.com>
2022-04-11 14:15:26 -0400

    style login page

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -27,11 +27,19 @@ export default function Login(props) {
 
     /* why did the passport session not work when this was a form? */
     return (
-    <div>
-      <h1>Login</h1>
-      <input type="text" placeholder='username' onChange={e => setUsername(e.target.value)}/>
-      <input type="text" placeholder='password' onChange={e => setPassword(e.target.value)} />
-      <button onClick={login}>Login</button>
+    <div className="mt-4 container-fluid">
+      <div className="row">
+        <div className="col-4"></div>
+        <div className="col-4">
+          <h2 className="mb-4">Login</h2>
+          <label className="mr-2" htmlFor="username">Username</label>
+          <input className="mb-3 form-control" type="text" onChange={e => setUsername(e.target.value)}/>
+          <label className="mr-2" htmlFor="password">Password</label>
+          <input className="mb-3 form-control" type="text" onChange={e => setPassword(e.target.value)} />
+          <button className="mt-3 btn btn-primary" onClick={login}>Login</button>
+        </div>
+        <div className="col-4"></div>
+        </div>
     </div>
     );
 }
