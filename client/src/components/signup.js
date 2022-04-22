bb31eada5613df3f564ad5d3d317b20f07841db6
essemd <sandimilicevic1991@gmail.com>
2022-04-22 14:08:41 -0400

    redo error text on signup

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -4,21 +4,28 @@ import axios from 'axios';
 export default function Signup(props) {
     const [username, setUsername] = useState(); 
     const [password, setPassword] = useState(); 
+    const [failed, setFailed] = useState(false);
 
       const signup = () => {
-        axios.post("http://localhost:5001/signup", {
+        axios.post('http://localhost:5001/signup', {
           username,
           password
         }, {
           withCredentials: true
-        }).then((res) => {
-          if (res.data === "success") {
-           window.location.href = "/login"
-         }
-        }, () => {
-            console.log("Failure");
         })
-      }
+        .then((res) => {
+          if (res.data === 'success') {
+              window.location.href = '/login'
+          } else if (res.data === 'exists') {
+              setFailed(true); 
+          } else {
+              window.alert('server error'); 
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      };
 
     return (
     <div className="mt-4 container-fluid">
@@ -31,6 +38,7 @@ export default function Signup(props) {
           <label className="mr-2" htmlFor="password">Password</label>
           <input className="mb-3 form-control" type="text" onChange={e => setPassword(e.target.value)} />
           <button className="mt-3 btn btn-primary" onClick={signup}>Signup</button>
+          {failed && <h6 className="mt-5 text-danger">Username already exists.</h6>}
         </div>
         <div className="col-4"></div>
         </div>
